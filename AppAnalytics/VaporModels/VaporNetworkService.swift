56ed918e66300d4c0107d664880e962fc9e1f725
Matt Schmulen <matt.schmulen@gmail.com>
2020-01-09 10:33:55 -0500

    metricKit

--- AppAnalytics/VaporModels/VaporNetworkService.swift
+++ AppAnalytics/VaporModels/VaporNetworkService.swift
@@ -4,9 +4,6 @@
 
 import Foundation
 
-// helpful references:
-// https://developer.apple.com/documentation/foundation/url_loading_system/uploading_data_to_a_website
-
 /**
  VaporNetworkService
  */
@@ -56,6 +53,7 @@ internal class VaporNetworkService<T:VaporModel> {
                 }
             }
         }
+        // task.priority = URLSessionTask.lowPriority
         task.resume()
     }//end createGeneric
     
@@ -85,139 +83,9 @@ internal class VaporNetworkService<T:VaporModel> {
             let models = try? JSONDecoder().decode([T].self, from: data)
             completion(models)
         })
+        // task.priority = URLSessionTask.lowPriority
         task.resume()
     }//end loadGeneric
-    
-    /**
-     Fetch a list of models with an upper limit and a specific sort
-     - Parameter limit: limit count
-     - Parameter page: page count
-     - Parameter withCompletion: completion handler
-     
-     GET http://localhost:8080/events?limit=20&page=10
-     
-     pagination reference
-     https://mihaelamj.github.io/Adding%20Pagination%20To%20Vapor%20Query/
-     
-     */
-//    public func fetch(limit:Int, page:Int, withCompletion completion: @escaping ([T]?) -> Void) {
-//        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: .main)
-//
-//        var components = URLComponents()
-//        components.scheme = AnalyticsService.shared().environment.URLScheme
-//        components.host = AnalyticsService.shared().environment.URLHost
-//        components.port = AnalyticsService.shared().environment.URLPort
-//        components.path = "/\(T.endpointRouteURL)"
-//        components.queryItems = [
-//            URLQueryItem(name: "limit", value: "\(limit)"),
-//            URLQueryItem(name: "page", value: "\(page)")
-//        ]
-//        let url = components.url!
-//
-//        // GET http://localhost:8080/events?limit=20&page=10
-//
-//        // url http://localhost:8080/events?limit=5&page=0
-//        // serverApps.count 6
-//
-//        let task = session.dataTask(with: url, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) -> Void in
-//            guard let data = data else {
-//                completion(nil)
-//                return
-//            }
-//
-//            // let debugJSON = try? JSONSerialization.jsonObject(with: data, options: [])
-//            // print(debugJSON)
-//
-//            let models = try? JSONDecoder().decode([T].self, from: data)
-//            completion(models)
-//        })
-//        task.resume()
-//    }//end fetch with limit
-    
 }
-
-
-
-
-
-
-        // -------------------------------
-
-        //let task = URLSession.shared.uploadTask(with: request, from: uploadData) { data, response, error in
-        //        let task = session.uploadTask(with: request, from: uploadData) { data, response, error in
-        //            if let error = error {
-        //                print ("error: \(error)")
-        //                return
-        //            }
-        //        }
-        //        task.resume()
-        
-//        let task = session.dataTask(with: url, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) -> Void in
-//            guard let data = data else {
-//                completion(nil)
-//                return
-//            }
-//            let model = try? JSONDecoder().decode(T.self, from: data)
-//            completion(model)
-//        })
-//        task.resume()
-
-    
-//    public func updateGeneric( model:T, withCompletion completion: @escaping (T?) -> Void) {
-//        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: .main)
-//        let url = URL(string:T.endpointURL)!
-//        let task = session.dataTask(with: url, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) -> Void in
-//            guard let data = data else {
-//                completion(nil)
-//                return
-//            }
-//            let model = try? JSONDecoder().decode(T.self, from: data)
-//            completion(model)
-//        })
-//        task.resume()
-//    }
-//
-//    public func deleteGeneric( model:T, withCompletion completion: @escaping (T?) -> Void) {
-//        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: .main)
-//        let url = URL(string:T.endpointURL)!
-//        let task = session.dataTask(with: url, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) -> Void in
-//            guard let data = data else {
-//                completion(nil)
-//                return
-//            }
-//            let model = try? JSONDecoder().decode(T.self, from: data)
-//            completion(model)
-//        })
-//        task.resume()
-//    }
-
-    // --------------------------------
-    
-    // let urlString = "http://localhost:8080/locations"
-    // let urlString = "http://localhost:8080/weatherInfos"
-
-//    public func fetch() {
-//
-//        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: .main)
-//        let url = URL(string: urlString)!
-//        let task = session.dataTask(with: url, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) -> Void in
-//            // Parse the data in the response and use it
-//        })
-//        task.resume()
-//    }
-    
-//    func loadWeatherInfo(withCompletion completion: @escaping ([WeatherInfo]?) -> Void) {
-//        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: .main)
-//        let url = URL(string:urlString)!
-//        let task = session.dataTask(with: url, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) -> Void in
-//            guard let data = data else {
-//                completion(nil)
-//                return
-//            }
-//            let models = try? JSONDecoder().decode([WeatherInfo].self, from: data)
-//            completion(models)
-//        })
-//        task.resume()
-//    }
 
 
